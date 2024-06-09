b536fda087e3830636014b5e6e1a6e3bdce53090
Alexandria P. <alxpettit@gmail.com>
2024-06-09 14:47:33 -0600

    flake freerouting

--- flake.nix
+++ flake.nix
@@ -13,8 +13,25 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages = flake-utils.lib.flattenTree {
+        packages = flake-utils.lib.flattenTree rec {
           freerouting = pkgs.callPackage ./pkgs/freerouting {};
+          default = freerouting;
+        };
+        devShels.default = pkgs.mkShell rec {
+          buildInputs = with pkgs;
+            [
+              (python3.withPackages (p:
+                with p; [
+                  kicad
+                ]))
+            ]
+            ++ (with packages; [
+              freerouting
+            ]);
+          KICAD7_SYMBOL_DIR = "${pkgs.kicad.libraries.symbols}/share/kicad/symbols";
+          KICAD7_FOOTPRINT_DIR = "${pkgs.kicad.libraries.footprints}/share/kicad/footprints";
+          KICAD_SYMBOL_DIR = KICAD7_SYMBOL_DIR;
+          KICAD_FOOTPRINT_DIR = KICAD7_FOOTPRINT_DIR;
         };
       }
     );
